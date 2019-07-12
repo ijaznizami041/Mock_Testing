cff523d7e58f7b5eb1efd4a83b85a3fda9c21537
Ijaz Nizami S <ZB9Z@eurofins.com>
2019-07-12 15:56:24 +0530

    initial commit : creting project

---
+++ Mock_Testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mock_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
