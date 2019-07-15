9d9e6d47b48b42f9b8a45b7858369e6a76be0702
Ijaz Nizami S <ZB9Z@eurofins.com>
2019-07-15 16:50:49 +0530

    Changed test project and wrote test cases

--- Mock_Testing.Tests/ProgramTest.cs
+++ Mock_Testing.Tests/ProgramTest.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using Xunit;
+
 
 namespace Mock_Testing.Tests
 {
     public class ProgramTest
     {
+        [Fact]
+        public void Add_WhenInputsAreCorrect_ShouldReturnTrue()
+        {
+            //Arrange
+            Program program = new Program();
+            int expected = 7;
+
+            //Act
+            int actual = program.Add(3,4);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- Mock_Testing/Program.cs
+++ Mock_Testing/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Mock_Testing.Tests")]
 namespace Mock_Testing
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program program = new Program();
+            Console.WriteLine(program.Add(5,6));
+        }
+
+        internal int Add(int a,int b)
+        {            
+            return a + b;
         }
     }
 }
